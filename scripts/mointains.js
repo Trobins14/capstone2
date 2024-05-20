9fb210b9984c5db8306515b0c5f1627f5cd8b6c3
trobins14 <tristan.robins14@gmail.com>
2024-05-20 14:38:57 -0400

    added necessary files for project

---
+++ scripts/mointains.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+
+mountainResults.innerHTML = "Hello"
+
+for(mountain of mountainArray){
+    mountainList.appendChild(mountainOption(mountain));
+}
+//mountainsArray
+
+//mountainList
+//mountainResults
+
+});//end loaded
